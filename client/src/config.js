6d719e7a7d8c0cfa3270e221d6b08a2b50097893
Burak Karamete <brkkrmt@gmail.com>
2022-04-19 01:33:32 +0300

    BASE URL edit

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
-export const axiosInstance = axios.create(
-    {baseUrl="https://sosyal-tohum.herokuapp.com/api"}
-)
+export const axiosInstance = axios.create({
+    baseURL = "https://sosyal-tohum.herokuapp.com/api/"
+})
