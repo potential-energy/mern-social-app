f2850a3a400242279f7bb14d06ae0d6c55de6876
Burak Karamete <brkkrmt@gmail.com>
2022-04-19 01:57:48 +0300

    naming fix

--- index.js
+++ index.js
@@ -48,10 +48,10 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
-app.use(express.static(path.join(__dirname, "/<front end app folder name>/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/<front end app folder name>/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
 });
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
